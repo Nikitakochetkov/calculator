ad0010f04075e6c872e4111509683a0155693963
Denız <deniz@Denzs-MacBook-Air.local>
2024-05-21 18:02:31 +0400

    Added filles to dev branch

--- src/main/java/org/example/ErrorHandling.java
+++ src/main/java/org/example/ErrorHandling.java
@@ -1,2 +1,24 @@
-package org.example;public class ErrorHandling {
+package org.example;
+
+public class ErrorHandling {
+
+    public static boolean isValidOperator(char operator) {
+        return operator == '+' || operator == '-' || operator == '*' || operator == '/';
+    }
+
+    public static boolean isDivisionByZero(double b, char operator) {
+        return operator == '/' && b == 0;
+    }
+
+    public static void handleNonNumericInput() {
+        System.out.println("Error! Non-numeric input.");
+    }
+
+    public static void handleInvalidOperator() {
+        System.out.println("Error! Invalid operator.");
+    }
+
+    public static void handleDivisionByZero() {
+        System.out.println("Error! Division by zero.");
+    }
 }
